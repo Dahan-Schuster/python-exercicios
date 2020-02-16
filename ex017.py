268138e63e32d34d7c75183510abc11fb1160c42
Dahan Schuster <dan.plschuster@gmail.com>
2020-02-16 12:25:48 -0300

    Exercício nº 17

---
+++ ex017.py
@@ -0,0 +1,3 @@
+from math import sin, cos, tan
+teta = float(input('Ângulo: '))
+print('---------------\nSeno: {0:.2f}°\nCosseno: {1:.2f}°\nTangente: {2:.2f}°'.format(sin(teta), cos(teta), tan(teta)))
