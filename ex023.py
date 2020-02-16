398e5e6ec190e4f3dc43df7986979f0f1ef01018
Dahan Schuster <dan.plschuster@gmail.com>
2020-02-16 12:25:48 -0300

    Exercício nº 22

---
+++ ex023.py
@@ -0,0 +1,7 @@
+cidade = input('Digite sua cidade: ')
+
+if cidade.upper().find('SANTO') == 0:
+    print('Sua cidade começa com \"Santo\"')
+
+else:
+    print('Sua cidade NÃO começa com \"Santo\"')
